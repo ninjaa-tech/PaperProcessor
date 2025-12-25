d93c704695e6cb303ed3d4c88a468fb1861bdbc6
reeey <reeey@NINJAAA>
2025-12-25 20:50:10 +1100

    Add project files.

---
+++ PaperProcessor/Data/PaperProcessorContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaperProcessor.Data
+{
+    public class PaperProcessorContext : DbContext
+    {
+        public PaperProcessorContext(DbContextOptions<PaperProcessorContext> options)
+            : base(options)
+        {
+        }
+
+        // We'll add DbSet<> here soon (Customers, WorkOrders, Materials, Invoices...)
+    }
+}
